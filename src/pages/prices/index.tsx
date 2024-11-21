42476f2d41e8b96a4d8f7596f894fc2a9497d40a
unknown <sshsultanov@gmail.com>
2024-11-21 22:33:14 +0200

    Update prices page

--- src/pages/prices/index.tsx
+++ src/pages/prices/index.tsx
@@ -1,7 +1,11 @@
-import { FC } from 'react';
+import { FC, useState } from "react";
 
-const Prices: FC = () => {
-  return <div>Prices</div>;
+const Prices: FC = (priceList) => {
+  const [isOpen, setOpen] = useState(false);
+  const toggleAccordion = () => {
+    setOpen(!isOpen);
+  };
+  return <section className="max-w-[1320px] w-full mx-auto"></section>;
 };
 
 export default Prices;
